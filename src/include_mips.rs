381cbdad41e4ef3e4ffd0bce8f8c661a88a6ce01
Joatin Granlund <granlundjoatin@gmail.com>
2022-03-17 10:11:26 +0100

    fix

--- src/include_mips.rs
+++ src/include_mips.rs
@@ -1,10 +1,10 @@
 #[macro_export]
 macro_rules! include_mips{
-    ($file_name:expr, $levels:expr)=>{
+    ($file_name:expr, $range:expr)=>{
         {
             let mut mip_maps = vec![];
-            for level in 0..$levels {
-                mip_maps.push(include_bytes!(concat!(env!("OUT_DIR"), "/", $file_name, "/mip_", level, ".png")))
+            for $level in $range {
+                mip_maps.push(include_bytes!(concat!(env!("OUT_DIR"), "/", $file_name, "/mip_", $level, ".png")))
             }
 
             mip_maps
