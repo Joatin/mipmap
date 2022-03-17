8ac857d9bb398529c5f680c0c06654b6a3505f1a
Joatin Granlund <granlundjoatin@gmail.com>
2022-03-17 08:54:06 +0100

    fix already created dir

--- src/generate_mipmaps_and_save.rs
+++ src/generate_mipmaps_and_save.rs
@@ -8,8 +8,8 @@ use std::error::Error;
 pub fn generate_mipmaps_and_save(original_image: &[u8], filter_type: FilterType, image_name: &str, save_path: &Path) -> Result<(), Box<dyn Error + Send + Sync>> {
     let images = generate_mipmaps(original_image, filter_type)?;
 
+    let _ = create_dir(save_path.join(format!("{}/", image_name)));
     for (index, image) in images.into_iter().enumerate() {
-        create_dir(save_path.join(format!("{}/", image_name)))?;
         let path = save_path.join(format!("{}/mip_{}.png", image_name, index));
         image.save_with_format(&path, ImageFormat::Png)?;
     }
